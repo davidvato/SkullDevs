4c23c2ea22e374b6dbaba646dbb31e3fd614d182
davidvato <39970099+davidvato@users.noreply.github.com>
2022-03-10 18:57:23 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(window).on("load", function () {
-    // makes sure the whole site is loaded
-    $("#status").fadeOut(); // will first fade out the loading animation
-    $("#preloader").delay(350).fadeOut("slow"); // will fade out the white DIV that covers the website.
-    $("body").delay(350).css({ overflow: "visible" });
+    // asegurarse de que todo el sitio cargue
+    $("#status").fadeOut(); // primero desvaneseremos la imagen de animacion
+    $("#preloader").delay(1000).fadeOut("slow"); // desvaneceremos el DIV que cubre todo el sitio
+    $("body").delay(1000).css({ overflow: "visible" });
   });
+  
